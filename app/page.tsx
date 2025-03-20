bccb74d17fa98657f1aa9ea93c1b83cdc882bc51
getFrontend <sergey10goals@gmail.com>
2025-03-20 22:32:07 +0200

    update: add logo & new favicon

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { useState } from "react"
 import { centeringMethods, type CenteringDirection } from "@/lib/center-data"
 import { CenterMethodCard } from "@/components/center-method-card"
 import { MethodSelector } from "@/components/method-selector"
+import Image from "next/image"
+import { logoImg } from "@/constants"
 
 export default function Home() {
   const [activeMethod, setActiveMethod] = useState(centeringMethods[0].id)
@@ -12,9 +14,10 @@ export default function Home() {
   const currentMethod = centeringMethods.find((method) => method.id === activeMethod)!
 
   return (
-    <main className="min-h-screen bg-background  sm:p-4 md:p-8">
+    <main className="min-h-screen bg-background sm:p-4 md:p-8">
       <div className="max-w-7xl mx-auto">
         <header className="mb-8 text-center">
+        <Image className="mx-auto my-2" src={logoImg} width={140} height={140} alt="Site logo: 10 Ways to Center a DIV" />
           <h1 className="text-4xl font-bold tracking-tight mb-2 p-2">10 Ways to Center a DIV</h1>
           <p className="text-muted-foreground max-w-2xl mx-auto">
             An interactive guide to different CSS methods for centering elements
