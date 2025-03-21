fc34834f00c4f4b6c4d7c1c888a54dc5223aee11
getFrontend <sergey10goals@gmail.com>
2025-03-21 12:14:51 +0200

    fix: github button style on hover

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
                 href="https://github.com/getFrontend" 
                 target="_blank" 
                 rel="noopener noreferrer"
-                className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background hover:bg-accent hover:text-accent-foreground h-10 w-10"
+                className="inline-flex items-center justify-center rounded-full text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background hover:bg-accent hover:text-accent-foreground h-10 w-10"
               >
                 <Github className="h-5 w-5" />
                 <span className="sr-only">GitHub</span>
